fed63aba2366ce66f8b54f743b53daf8c58f9d26
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-29 14:29:54 +0900

    Add graphql query to jobs and fetch the data

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,33 @@
-import React from "react"
-import Title from "./Title"
-import { FaAngleDoubleRight } from "react-icons/fa"
-import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
+import React from "react";
+import Title from "./Title";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import { graphql, useStaticQuery } from "gatsby";
+import { Link } from "gatsby";
+
+const query = graphql`
+  {
+    allStrapiJob {
+      nodes {
+        data {
+          attributes {
+            company
+            position
+            date
+            desc {
+              name
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
-}
+  const data = useStaticQuery(query);
+  console.log(data);
+  return <h2>jobs component</h2>;
+};
 
-export default Jobs
+export default Jobs;
