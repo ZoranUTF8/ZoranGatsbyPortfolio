80e78d4d049ec5388ad8a2834a24300554b89040
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-30 14:05:44 +0900

    set up projects page

--- src/assets/projectsData/ProjectsData.js
+++ src/assets/projectsData/ProjectsData.js
@@ -1,95 +1,144 @@
 const projectData = [
   {
     id: 1,
-    title: "React Chat",
-    slug: "react-chat",
-    description: "Realtime Chat Application.",
-    github: "https://github.com/ZoranUTF8/react-chat-app",
-    url: "https://nas-cat.netlify.app/",
+
+    title: "React web store",
+    slug: "react-web-store",
+    description: "React web store with Auth0 and Stripe payment.",
+    github: "https://github.com/ZoranUTF8/react_web_shop",
+    url: "https://moja-trgovina.netlify.app/",
+    imageUrl: "https://i.ibb.co/xGtxgf0/Screenshot-2021-12-30-at-13-20-45.png",
     featured: true,
+    stack: [
+      { name: "React", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
+  },
+  {
+    id: 2,
+
+    title: "Yelp camp",
+    slug: "yelp-camp",
+    description: "Yelp camp clone. Publish your campground online.",
+    github: "https://github.com/ZoranUTF8/yelpCampClone",
+    url: "https://boiling-basin-15012.herokuapp.com/",
+    imageUrl: "https://zoranjanjic-portofolio.netlify.app/images/yelpcamp.png",
+    featured: true,
+    stack: [
+      { name: "Node", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
   },
   {
     id: 3,
 
-    title: "Yelp camp",
-    slug: "yelp-camp",
-    description: "Yelp camp clone. Publish your campground.",
-    github: "https://github.com/ZoranUTF8/yelpCampClone",
-    url: "https://boiling-basin-15012.herokuapp.com/",
+    title: "Simple recipes",
+    slug: "simple-recipes",
+    description: "Website for posting cooking recipes.",
+    github: "https://github.com/ZoranUTF8/Simple-recipes-gatsby.js",
+    url: "https://jednostavnirecepti.netlify.app",
+    imageUrl: "https://i.ibb.co/zQZn6CZ/Screenshot-2021-12-30-at-13-14-23.png",
     featured: true,
+    stack: [
+      { name: "React", id: 1 },
+      { name: "Javascript", id: 2 },
+      { name: "Gatsby", id: 3 },
+    ],
   },
+
   {
     id: 4,
-
-    title: "Photo search",
-    slug: "photo-search",
-    description: "Photo search with infinite scroll.",
-    github: "https://github.com/ZoranUTF8/photoScroll",
-    url: "https://infinitephotoscroll.netlify.app/",
-    featured: false,
-  },
-  {
-    id: 5,
 
     title: "Hacker news",
     slug: "hacker-news",
     description: "Hacker News API implementation.",
     github: "https://github.com/ZoranUTF8/hacker_news_react_API",
     url: "https://hacker-news-react-api.netlify.app/",
+    imageUrl:
+      "https://zoranjanjic-portofolio.netlify.app/images/hacker-news.png",
     featured: true,
+    stack: [
+      { name: "React", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
   },
   {
-    id: 6,
+    id: 5,
 
     title: "Movie Search",
     slug: "movie-search",
     description: "Movie database search API implementation.",
     github: "https://github.com/ZoranUTF8/movieDB_react",
     url: "https://search-moviedb-react.netlify.app/",
-    featured: false,
+    imageUrl:
+      "https://zoranjanjic-portofolio.netlify.app/images/movie-search.png",
+    featured: true,
+    stack: [
+      { name: "React", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
+  },
+  {
+    id: 6,
+
+    title: "Weather Forecast",
+    slug: "weather-forecast",
+    description: "Weather page using OpenWeatherMap API.",
+    github: "https://github.com/ZoranUTF8/WeatherApi",
+    url: "https://mighty-brushlands-35689.herokuapp.com/",
+    imageUrl: "https://zoranjanjic-portofolio.netlify.app/images/weather.png",
+    featured: true,
+    stack: [
+      { name: "Node", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
   },
   {
     id: 7,
 
-    title: "Weather Forecast",
-    slug: "weather-forecast",
-    description: "Simple weather page using OpenWeatherMap API.",
-    github: "https://github.com/ZoranUTF8/WeatherApi",
-    url: "https://mighty-brushlands-35689.herokuapp.com/",
+    title: "Restaurant",
+    slug: "restaurant-website",
+    description: "Restaurant  website.",
+    github: "https://github.com/ZoranUTF8/fabrika_ukusa_webPage",
+    url: "https://zoranutf8.github.io/fabrika_ukusa_webPage/",
+    imageUrl:
+      "https://zoranjanjic-portofolio.netlify.app/images/fabrikaukusa.png",
     featured: false,
+    stack: [{ name: "HTML", id: 1 }],
   },
   {
     id: 8,
 
-    title: "Restaurant website.",
-    slug: "restaurant-website",
-    description: "Restaurant website.",
-    github: "https://github.com/ZoranUTF8/fabrika_ukusa_webPage",
-    url: "https://zoranutf8.github.io/fabrika_ukusa_webPage/",
+    title: "Package sale",
+    slug: "package-sale",
+    description: "Internet package sale desktop application.",
+    github: "https://github.com/ZoranUTF8/Internet-Package-App",
+    url: "https://github.com/ZoranUTF8/Internet-Package-App",
+    imageUrl:
+      "https://zoranjanjic-portofolio.netlify.app/images/internetsales.png",
     featured: false,
+    stack: [
+      { name: "Java", id: 1 },
+      { name: "Hibernate", id: 2 },
+    ],
   },
   {
     id: 9,
-
-    title: "Package sale",
-    slug: "package-sale",
-    description: "Internet package sale",
-    github: "https://github.com/ZoranUTF8/Internet-Package-App",
-    url: "https://github.com/ZoranUTF8/Internet-Package-App",
-    featured: false,
-  },
-  {
-    id: 10,
 
     title: "Job adds API",
     slug: "job-adds-api",
     description: "API with JSON Web Token auth.",
     github: "https://github.com/ZoranUTF8/Jobs_API/blob/main/package.json",
     url: "https://jobs-api-zoran.herokuapp.com/api-docs/",
+    imageUrl: "https://zoranjanjic-portofolio.netlify.app/images/jobapi.png",
     featured: false,
+    stack: [
+      { name: "Node", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
   },
   {
-    id: 11,
+    id: 10,
 
     title: "Store API",
     slug: "store-api",
@@ -97,7 +146,43 @@ const projectData = [
       "Advanced filtering,sorting, dynamically populating db. Store API with products that enables easy filtering and searching their DB. ",
     github: "https://github.com/ZoranUTF8/store_API/tree/main",
     url: "https://jobs-api-zoran.herokuapp.com/api-docs/",
+    imageUrl: "https://zoranjanjic-portofolio.netlify.app/images/inventory.png",
     featured: false,
+    stack: [
+      { name: "Node", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
+  },
+  {
+    id: 11,
+
+    title: "Photo search",
+    slug: "photo-search",
+    description: "Photo search with infinite scroll.",
+    github: "https://github.com/ZoranUTF8/photoScroll",
+    url: "https://infinitephotoscroll.netlify.app/",
+    imageUrl:
+      "https://zoranjanjic-portofolio.netlify.app/images/photo-search.png",
+    featured: false,
+    stack: [
+      { name: "React", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
+  },
+
+  {
+    id: 12,
+    title: "React Chat",
+    slug: "react-chat",
+    description: "Realtime Chat Application using socket.io.",
+    github: "https://github.com/ZoranUTF8/react-chat-app",
+    url: "https://nas-cat.netlify.app/",
+    imageUrl: "https://zoranjanjic-portofolio.netlify.app/images/chatApp.png",
+    featured: true,
+    stack: [
+      { name: "React", id: 1 },
+      { name: "Javascript", id: 2 },
+    ],
   },
 ];
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,43 @@
-import React from "react"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-import { Link } from "gatsby"
+import React from "react";
+// import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { FaGithubSquare, FaShareSquare } from "react-icons/fa";
+import { Link } from "gatsby";
 
-const Project = () => {
-  return <h2>single project component</h2>
-}
+const Project = ({
+  title,
+  slug,
+  description,
+  github,
+  url,
+  stack,
+  imageUrl,
+  index,
+}) => {
+  return (
+    <article className="project">
+      <img src={imageUrl} alt={title} className="project-img" />
+      <div className="project-info">
+        <span className="project-number">0{index + 1}.</span>
+        <Link to={`/projects/${slug}`} className="project-slug">
+          <h3>{title}</h3>
+        </Link>
+        <p className="project-desc">{description}</p>
+        <div className="project-stack">
+          {stack.map((tech) => {
+            return <span key={tech.id}>{tech.name}</span>;
+          })}
+        </div>
+        <div className="project-links">
+          <a href={github}>
+            <FaGithubSquare className="project-icon"></FaGithubSquare>
+          </a>
+          <a href={url}>
+            <FaShareSquare className="project-icon"></FaShareSquare>
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
 
-export default Project
+export default Project;
