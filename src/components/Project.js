260644c8959cc32004ce31fad889b64e933ae4ca
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-30 17:39:43 +0900

    mSet up error page

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,7 @@ import React from "react";
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa";
 import { Link } from "gatsby";
 
+
 const Project = ({
   title,
   slug,
@@ -19,9 +20,10 @@ const Project = ({
       <img src={imageUrl} alt={title} className="project-img" />
       <div className="project-info">
         <span className="project-number">0{index + 1}.</span>
-        <Link to={`/projects/${slug}`} className="project-slug">
+
+       
           <h3>{title}</h3>
-        </Link>
+        
         <p className="project-desc">{description}</p>
         <div className="project-stack">
           {stack.map((tech) => {
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,31 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Title from "../components/Title"
-import Seo from "../components/Seo"
+import React from "react";
+import Title from "../components/Title";
+import Seo from "../components/Seo";
+import heroImg from "../assets/images/hero.svg";
 
 const About = () => {
   return (
-    <>
-      <h2>about page</h2>
-    </>
-  )
-}
+    <section className="about-page">
+      <div className="section-center about-center">
+        <img src={heroImg} alt="hero img" className="about-img-svg" />
+        <article className="about-text">
+          <h3>About me</h3>
+          <p>
+            Hello, my name is Zoran. I'm looking for a job or internship
+            opportunity. I have been using the following technologies: Java,
+            JavaScript,React.js,Gatsby.js, and Node.js. I studied Java
+            development at ITAcademy in Belgrade(Serbia). Here I had the
+            opportunity to acquire the Java Developer Certificate. I received a
+            couple of certificates for Full Stack Web Development on Udemy.com
+            as well. Currently, I'm pursuing a bachelor's degree in computer
+            science at the University of the people. I have a strong will to
+            learn new technologies and I aspire to be a FullStack developer in
+            the future.
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
